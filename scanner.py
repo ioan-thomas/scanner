b1244fc36ea3360ef183d541a08938eabfee0c38
ioan-thomas <ioanthomas21@gmail.com>
2023-05-01 12:36:57 +0100

    feat: main class now calls other to parse args

--- scanner.py
+++ scanner.py
@@ -2,16 +2,18 @@ import socket
 from concurrent.futures import ThreadPoolExecutor
 import re
 import argparse
+
 class PortScanner:
-    def __init__(self, target_host, start_port, end_port, timeout=1):
-        self.__target_host = target_host
-        self.__start_port = start_port
-        self.__end_port = end_port
-        self.__timeout = timeout
+    def __init__(self, args):
+        self.__target_host = args.host
+        self.__start_port = args.start_port
+        self.__end_port = args.end_port
+        self.__timeout = args.timeout
+        self.__max_workers = args.threads
 
     def scan(self):
         open_ports = []
-        with ThreadPoolExecutor(max_workers=100) as executor:
+        with ThreadPoolExecutor(max_workers=self.__max_workers) as executor:
             futures = []
             for port in range(self.__start_port, self.__end_port + 1):
                 future = executor.submit(self.__scan_port, port)
@@ -25,8 +27,8 @@ class PortScanner:
     def __scan_port(self, port):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(self.timeout)
-            result = sock.connect_ex((self.target_host, port))
+            sock.settimeout(self.__timeout)
+            result = sock.connect_ex((self.__target_host, port))
 
             if result == 0:
                 print(f"Port {port}: Open")
@@ -38,39 +40,43 @@ class PortScanner:
         finally:
             sock.close()
 
+
 class PortScannerArgs:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="Port scanner")
-        self.parser.add_argument("-h", "--host",help="Host to scan")
-        self.parser.add_argument("start_port", help="The Start port [1-65535]", type=self.valid_port, metavar="Start_Port[1-65535]")
-        self.parser.add_argument("end_port", help="The End Port [1-65535]", type=self.valid_port, metavar="End_Port[1-65535]")
+        self.parser.add_argument("host", type=self.valid_hostname, help="Host to scan")
+        self.parser.add_argument("start_port", type=self.valid_port, help="The Start port [1-65535]", metavar="Start_Port[1-65535]")
+        self.parser.add_argument("end_port", type=self.valid_port, help="The End Port [1-65535]", metavar="End_Port[1-65535]")
         self.parser.add_argument("--timeout", help="Timeout (seconds)", type=int, default=1)
         self.parser.add_argument("-t","--threads", help="The number of threads to use for the port scans (default: 1, max: 100)", type=self.valid_threads, default=1)
 
     def parse_args(self):
         return self.parser.parse_args()
 
-    @staticmethod
-    def valid_port(value):
+    def valid_port(self, value):
         port = int(value)
         if port < 1 or port > 65535:
-             raise argparse.ArgumentTypeError(f"Invalid port number: {value}")
+            raise argparse.ArgumentTypeError(f"Invalid port number: {value}")
         return port
 
-    @staticmethod
-    def valid_threads(value):
+    def valid_threads(self, value):
         num_of_threads = int(value)
         if num_of_threads < 1 or num_of_threads > 100:
-             raise argparse.ArgumentTypeError(f"Invalid number of threads: {value}. Please choose a value between 1-100")
+            raise argparse.ArgumentTypeError(f"Invalid number of threads: {value}. Please choose a value between 1-100")
         return num_of_threads
 
-if __name__ == "__main__":
-    target_host = "www.hackthissite.org"
-    start_port = 1
-    end_port = 65535
+    def valid_hostname(self, hostname):
+        pattern = r"^(?=.{1,255}$)[0-9a-zA-Z]([-\w]*[0-9a-zA-Z])*(\.[0-9a-zA-Z]([-\w]*[0-9a-zA-Z])*)+$"
+        if not re.match(pattern, hostname):
+            raise argparse.ArgumentTypeError(f"Invalid hostname: {hostname}")
+        return hostname
 
-    target_ip = socket.gethostbyname(target_host)
-    scanner = PortScanner(target_ip, start_port, end_port)
+if __name__ == "__main__":
+    arg_parser = PortScannerArgs()
+    args = arg_parser.parse_args()
+
+    target_ip = socket.gethostbyname(args.host)
+    scanner = PortScanner(args)
     open_ports = scanner.scan()
     print(f"Open ports: {open_ports}")
     print(target_ip)
