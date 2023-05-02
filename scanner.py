becd1c0f2fe9db7de11ae506aa998739bbd58e72
ioan-thomas <ioanthomas21@gmail.com>
2023-05-02 09:19:54 +0100

    fix: user can now input range of ports correctly

--- scanner.py
+++ scanner.py
@@ -5,6 +5,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed  # for concurren
 import argparse                    # for parsing the command-line arguments
 from tqdm import tqdm              # for the progress bar
 from vulnerable_ports import TOP_VULN_PORTS # for the list of top vulnerable ports
+import re                          # for validating the port range
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)  # set the logging level to INFO so that only INFO and higher levels are logged
@@ -52,33 +53,38 @@ class PortScanner:
     # Scan the open ports for the specified host
     def __scan_host(self, target_ip):
         open_ports = []
-        with ThreadPoolExecutor(max_workers=self.__max_workers) as executor:
-            # Determine which ports to scan based on the user's input
-            futures = {executor.submit(self.__scan_port, target_ip, port): 
-                    port for port in (self.__vuln_ports 
-                                        if self.__scan_vuln_ports 
-                                        else range(self.__start_port, self.__end_port + 1)
-                                        )
-                        }
-            try:
-                # Use tqdm to show a progress bar while the port scans are in progress
-                with tqdm(total=len(futures), 
-                        desc=f"Scanning {target_ip}", 
-                        ncols=100, unit="port") as progress:
-                    # Loop through each port being scanned and check if it's open
-                    for future in as_completed(futures):
-                        open_port = future.result()
-                        if open_port is not None:
-                            open_ports.append(open_port)
-                        progress.update()
-            except KeyboardInterrupt:
-                # If the user presses `Ctrl + C`, stop the scan and log a warning message
-                logger.warning("Stopping scan... Please wait for threads to finish.")
-                executor._threads.clear()
-            except Exception as e:
-                # If there's an exception during the port scan, log an error message
-                logger.exception(f"Error: {e}")
-        return open_ports
+        try: 
+            with ThreadPoolExecutor(max_workers=self.__max_workers) as executor:
+                # Determine which ports to scan based on the user's input
+                futures = {executor.submit(self.__scan_port, target_ip, port): 
+                        port for port in (self.__vuln_ports 
+                                            if self.__scan_vuln_ports 
+                                            else range(self.__start_port, self.__end_port + 1)
+                                            )
+                            }
+                try:
+                    # Use tqdm to show a progress bar while the port scans are in progress
+                    with tqdm(total=len(futures), 
+                            desc=f"Scanning {target_ip}", 
+                            ncols=100, unit="port") as progress:
+                        # Loop through each port being scanned and check if it's open
+                        for future in as_completed(futures):
+                            open_port = future.result()
+                            if open_port is not None:
+                                open_ports.append(open_port)
+                            progress.update()
+                except KeyboardInterrupt:
+                    # If the user presses `Ctrl + C`, stop the scan and log a warning message
+                    logger.warning("Stopping scan... Please wait for threads to finish.")
+                    executor._threads.clear()
+                except Exception as e:
+                    # If there's an exception during the port scan, log an error message
+                    logger.exception(f"Error: {e}")
+            return open_ports
+        
+        except Exception as e:
+            logger.exception(f"Error: {e}")
+        
 
     def __scan_port(self, target_ip, port):
         # Connect to the specified port and return the port number if it's open
@@ -116,27 +122,48 @@ class PortScannerArgs:
 
         # create a mutually exclusive group for the start and end ports - only one of them can be specified
         ports_group = self.__parser.add_mutually_exclusive_group(required=True)
-        ports_group.add_argument("-p", "--port-range",type=argparse.Range(1, 65535), help="The Port Range [1-65535]", metavar="Start_Port-End_Port", choices=range(1, 65536))
-        ports_group.add_argument("--scan-vuln-ports", help="Scan the top vulnerable ports (default: True)", action="store_true", default=True)
+        ports_group.add_argument("-p", "--port-range",type=str, help="The Port Range [1-65535]", metavar="Start_Port-End_Port")
+        ports_group.add_argument("--scan-vuln-ports", help="Scan the top vulnerable ports (default: False)", action="store_true", default=False)
 
         self.__parser.add_argument("--timeout", help="Timeout (seconds) for DNS and connecting to ports", type=int, default=3)
-        self.__parser.add_argument("-t", "--threads", help="The number of threads to use for the port scans (default: 1, max: 100)", type=int, default=100, choices=range(1, 101))
+        self.__parser.add_argument("-t", "--threads", help="The number of threads to use for the port scans (default: 1, max: 100)", type=int, default=100)
         self.__parser.add_argument("-v", dest="verbose" , help="Show verbose output", action="store_true", default=False)
         self.__parser.add_argument("-o", "--output-file", dest="output",help="The output file path to write results to (default: None)", default=None)
+        
         
     def parse_args(self):
         # parsing the arguments
         args = self.__parser.parse_args()
 
-
         if args.port_range:
-            start_port, end_port = map(int, args.port_range.split('-'))
-            args.start_port = start_port
-            args.end_port = end_port
+            try:
+                start_port, end_port = self.__parse_port_range(args.port_range)
+                args.start_port = start_port
+                args.end_port = end_port
+            except argparse.ArgumentTypeError as e:
+                self.__parser.error(str(e))
         else:
             args.start_port = min(TOP_VULN_PORTS)
             args.end_port = max(TOP_VULN_PORTS)
         return args
+    
+    def __parse_port_range(self, port_range_str):
+        # Use regular expressions to parse the port range string
+        port_range_regex = r'^(\d{1,5})-(\d{1,5})$'
+        match = re.match(port_range_regex, port_range_str)
+        if match:
+            start_port = int(match.group(1))
+            end_port = int(match.group(2))
+            # Make sure start_port <= end_port
+            if start_port > end_port:
+                start_port, end_port = end_port, start_port
+            # Make sure start_port >= 1 and end_port <= 65535
+            start_port = max(start_port, 1)
+            end_port = min(end_port, 65535)
+        else:
+            raise argparse.ArgumentTypeError(f"Invalid port range: {port_range_str}. Port range should be in the format 'start_port-end_port', where start_port and end_port are integers between 1 and 65535.")
+
+        return start_port, end_port
     
 # runs the program if it's executed directly i.e. in the main module
 if __name__ == "__main__":
